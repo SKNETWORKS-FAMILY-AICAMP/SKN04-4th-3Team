1e362cabfcaaec5e416de4ef740a52a5cf222fee
hwarange <asdf78964@naver.com>
2024-12-05 11:51:09 +0900

    migrate 추가

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -7,3 +7,6 @@ sudo docker-compose down
 
 echo "> 🟢 Run new docker services."
 sudo docker-compose up -d --build >> /var/log/deploy.log 2>&1
+
+echo "> 🟢 Apply database migrations."
+sudo docker-compose exec backend python manage.py migrate >> /var/log/deploy.log 2>&1
