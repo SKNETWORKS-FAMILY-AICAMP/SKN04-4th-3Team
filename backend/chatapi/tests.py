6970e0c318d66c654cae64d16ce8d779110d0b5e
kohyourim <gusgmltnr6811@gmail.com>
2024-12-05 10:21:02 +0900

    feat: tests.py 추가

--- backend/chatapi/tests.py
+++ backend/chatapi/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+API_URL = "${window.location.protocol}//${window.location.host}/api/chat/"
+
+class ChatAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse(API_URL)  
+
+    def test_send_message(self):
+        response = self.client.post(self.url, {'message': '너는 누구니?'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('bot_response', response.data)
+
+    def test_empty_message(self):
+        response = self.client.post(self.url, {'message': ''}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
